4a618b3d5693a5b3364c768e54a04103e3661b48
Khanieiev Ivan <75529688+CatBook2010@users.noreply.github.com>
2022-04-05 10:19:06 +0000

    Triangle.1.0 is working

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from triangle import Triangle
 
 correct_sides = False
 while not correct_sides:
-    side_1 = float(input("Enter side1: "))
-    side_2 = float(input("Enter side2: "))
-    side_3 = float(input("Enter side3: "))
+    side1 = float(input("Enter side1: "))
+    side2 = float(input("Enter side2: "))
+    side3 = float(input("Enter side3: "))
 
-    correct_sides = side_1 + side_2 > side_3 and side_2 + side_3 > side_1 and side_1 + side_3 > side_2
+    correct_sides = side1 + side2 > side3 and side2 + side3 > side1 and side1 + side3 > side2
 
     if not correct_sides:
         print("Please, enter correct sides.")
@@ -14,7 +14,7 @@ while not correct_sides:
 color = input("Enter color: ")
 filled = bool(int(input("Enter 1/0 for filled (1: true, 0: false): ")))
 
-new_triangle = Triangle(color, filled, side_1, side_2, side_3)
+new_triangle = Triangle(color, filled, side1, side2, side3)
 
 print(f"The perimeter is {new_triangle.getPerimeter()}")
 print(f"Color is {color}") 
